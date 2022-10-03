a74b061f86163f36c75d33407433709b9e89f6d9
SoulTheDevt <souleymane.gueye.pro@gmail.com>
2022-10-03 23:42:32 +0200

    last commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 //imports
 import express from "express"
 import mysql from "mysql"
+import cors from "cors"
 
 const app = express()
 
@@ -14,6 +15,7 @@ host: "localhost",
 
 //middleware
 app.use(express.json())
+app.use(cors())
 
 
 
@@ -30,14 +32,39 @@ app.get("/books", (req,res) => {
 })
 
 app.post("/books", (req,res) => {
-    const query = "INSERT INTO books (`title`, `desc`, `cover`) VALUES(?) "
+    const query = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES(?) "
     const values = [
         req.body.title,
         req.body.desc,
+        req.body.price,
         req.body.cover]
     db.query(query,[values],(err, data) => {
         if(err) return res.json(err);
         return res.json("Success")
+    })
+})
+
+app.delete("/books/:id", (req,res) => {
+    const query = "DELETE FROM books WHERE id = ? "
+    const values = req.params.id
+    db.query(query,[values],(err, data) => {
+        if(err) return res.json(err);
+        return res.json("Deleted successfully")
+    })
+})
+
+app.put("/books/:id", (req,res) => {
+    const query = "UPDATE books SET `title` = ?, `desc`= ?, `price` = ?, `cover`= ? WHERE id = ?"
+    const bookId = req.params.id;
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.price,
+        req.body.cover,
+    ]
+    db.query(query,[...values,bookId],(err, data) => {
+        if(err) return res.json(err);
+        return res.json("Updated successfully")
     })
 })
 
